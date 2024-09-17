4b8e0466d4264fe396638be49b2489a766c731c5
Đào Bảo Đại <89094892+JackFrost1412@users.noreply.github.com>
2024-09-17 14:27:58 +0700

    clean

--- Output/SQL_Job_DS/FNC_SVC_AR_FCT_FA/PAR_DMT_TWT_FNC_SVC_AR_ANL_FCT_FA_LST_DY_TruncIns.sql
+++ Output/SQL_Job_DS/FNC_SVC_AR_FCT_FA/PAR_DMT_TWT_FNC_SVC_AR_ANL_FCT_FA_LST_DY_TruncIns.sql
@@ -12,6 +12,6 @@ SELECT
 	,ORIG_CLS_BAL_MTD_LCY
 FROM FNC_SVC_AR_ANL_FCT_FA FCT
 LEFT JOIN AC_AR_DIM ON AC_AR_DIM.AC_AR_DIM_ID = FCT.AC_AR_DIM_ID
-WHERE FCT.CDR_DT_DIM_ID = to_number(to_char(to_date(:pDate:, 'yyyymmdd')-1, 'yyyymmdd'))
+WHERE FCT.CDR_DT_DIM_ID = to_number(to_char(to_date(:pdate:, 'yyyymmdd')-1, 'yyyymmdd'))
 AND FCT.SRO_DIM_ID = 7;
 
