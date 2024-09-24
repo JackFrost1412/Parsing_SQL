3689f02f7479c9655b4b5bc19cb1be8be5d3dc44
Đào Bảo Đại <89094892+JackFrost1412@users.noreply.github.com>
2024-09-24 15:26:37 +0700

    he

--- SourceCode/Extract_Job_DataStage/Job_DS_to_sql.py
+++ SourceCode/Extract_Job_DataStage/Job_DS_to_sql.py
@@ -41,7 +41,7 @@ def extract_jobs_to_sql(dsx_content, output_dir):
     total_jobs = 0
     
     # Danh sách các từ khóa bắt đầu lọc câu truy vấn
-    sql_keywords = ['SELECT', 'UPDATE', 'INSERT INTO', 'DELETE FROM', 'WITH', 'TRUNCATE', 'DROP', 'CREATE']
+    sql_keywords = ['SELECT', 'UPDATE', 'INSERT INTO', 'DELETE FROM', 'WITH', 'TRUNCATE', 'DROP', 'CREATE', 'MERGE', 'ALTER', 'EXECUTE']
 
     # Process each query and assign it to the corresponding job
     for sql_pos, query in sql_queries:
